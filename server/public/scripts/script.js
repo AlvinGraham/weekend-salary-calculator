45b601a64ee3c15b3c2447d9b81c33f519de81be
Alvin Graham <grah0071@gmail.com>
2023-12-10 06:47:51 -0600

    Create HTML Table Structure

--- server/public/scripts/script.js
+++ server/public/scripts/script.js
@@ -4,3 +4,10 @@ function submitBtnClk(event) {
   event.preventDefault();
   console.log('Submit Button Handler function');
 }
+
+function deleteRowBtn(event) {
+  console.log('Delete row');
+  console.log(event.target);
+  console.log(event.target.parentElement.parentElement)
+  event.target.parentElement.parentElement.remove();
+}

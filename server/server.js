69db47f3829bc185963153d1e9d1629c3ac0ef1e
Alvin Graham <grah0071@gmail.com>
2023-12-10 13:01:42 -0600

    Add server.js functionality

--- server/server.js
+++ server/server.js
@@ -1,13 +1,13 @@
-// Require express - give us a function
+// invoke requires on express and dotenv
 const express = require('express');
 require('dotenv').config();
 
 
 // Create an instance of express by calling the function returned above - gives us an object
 const app = express();
-const port = process.env.PROD_PORT;// || 5001;
+const port = process.env.PORT || 5001;
 
-console.log(process.env);
+// console.log(process.env);
 
 // express static file serving - public is the folder name
 app.use(express.static('server/public'));
@@ -17,6 +17,6 @@ app.listen(port, () => {
   console.log('listening on port', port);
 });
 
-app.get('/hello', (req, res) => {
-  res.send('Hello World!');
-});
+// app.get('/hello', (req, res) => {
+//   res.send('Hello World!');
+// });
